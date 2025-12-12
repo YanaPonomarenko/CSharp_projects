044d34e9dccbda2efe10ac03361b9621fa1d3489
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-12-12 19:41:07 +0200

    added files

---
+++ Less_classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Less_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee emp= new Employee();
+
+            emp.Name = "Ivan";
+            emp.Age = 20;
+            emp.Salary = 1500;
+
+            emp.Show();
+        }
+    }
+}
