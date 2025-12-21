a6bc57b59a0fef6f6647b5cf1f0e2781f112c664
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-12-21 13:34:08 +0200

    added h/w 8.12

---
+++ homework3.2/Journal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3._2
+{
+    internal class Journal
+    {
+        public string Title { get; set; }
+        private int year;
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Journal(string t, int y, string d, string p, string e)
+        {
+            Title = t;
+            Year = y;
+            Description = d;
+            Phone = p;
+            Email = e;
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 0) year = 0;
+                else year = value;
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"\nЖурнал: {Title}");
+            Console.WriteLine($"Рік: {Year}");
+            Console.WriteLine($"Опис: {Description}");
+            Console.WriteLine($"Телефон: {Phone}");
+            Console.WriteLine($"Email: {Email}");
+        }
+    }
+}
---
+++ homework3.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace homework3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal j = new Journal("Подорожі", 2000, "Журнал про подорожі", "+380123456789", "travel@mail.com");
+        }
+    }
+}
