cbed087b994c96aa7a99fd18c3ed7bf42d4bcb4b
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-12-21 13:32:32 +0200

    added h/w 8.12

---
+++ homework3.3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace homework3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop s = new Shop("Продукти", "вул. Дерибасівська, 10", "Продуктовий магазин", "123-456", "shop@mail.com");
+            s.Show();
+        }
+    }
+}
---
+++ homework3.3/Shop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3._3
+{
+    internal class Shop
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Shop()
+        {
+        }
+
+        public Shop(string n, string a, string d, string p, string e)
+        {
+            Name = n;
+            Address = a;
+            Description = d;
+            Phone = p;
+            Email = e;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"\nМагазин: {Name}");
+            Console.WriteLine($"Адреса: {Address}");
+            Console.WriteLine($"Опис: {Description}");
+            Console.WriteLine($"Телефон: {Phone}");
+            Console.WriteLine($"Email: {Email}");
+        }
+    }
+}
