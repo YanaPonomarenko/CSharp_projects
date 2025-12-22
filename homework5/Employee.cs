614b97126b8fe9ef38ed03f6d4d239ce03658b66
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-12-22 15:47:36 +0200

    added h/w 12.12

---
+++ homework5/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, int age, int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Name}, {Age} years, ${Salary}");
+        }
+
+        public static Employee operator +(Employee emp, int amount)
+        {
+            emp.Salary += amount;
+            return emp;
+        }
+
+        //для зменшення 
+        public static Employee operator -(Employee emp, int amount)
+        {
+            emp.Salary -= amount;
+            return emp;
+        }
+
+        //перевірка
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary == emp2.Salary;
+        }
+
+        //перевірка 
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary != emp2.Salary;
+        }
+
+        //порівняння 
+        public static bool operator >(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary > emp2.Salary;
+        }
+
+        //порівняння 
+        public static bool operator <(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary < emp2.Salary;
+        }
+    }
+}
---
+++ homework5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Ivan", 20, 1500);
+            Employee emp2 = new Employee("Anna", 35, 6000);
+
+            Console.WriteLine("Start:");
+            emp1.Show();
+            emp2.Show();
+
+            emp1 = emp1 + 1000;
+            Console.WriteLine("\nAfter +1000:");
+            emp1.Show();
+
+            emp2 = emp2 - 500;
+            Console.WriteLine("After -500:");
+            emp2.Show();
+
+            Console.WriteLine($"\nemp1 == emp2: {emp1 == emp2}");
+            Console.WriteLine($"emp1 != emp2: {emp1 != emp2}");
+            Console.WriteLine($"emp1 > emp2: {emp1 > emp2}");
+            Console.WriteLine($"emp1 < emp2: {emp1 < emp2}");
+        }
+    }
+}
