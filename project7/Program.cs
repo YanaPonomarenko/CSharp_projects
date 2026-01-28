f2102f0efd1721fa10aed7efffd81145335fbc39
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-01-28 17:39:15 +0200

    class/w 09.01

---
+++ project7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+namespace project7
+{
+    class Pair<T1, T2>
+    {
+        private T1 a;
+        private T2 b;
+
+        public void Add(T1 value1, T2 value2)
+        {
+            a = value1;
+            b = value2;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"First: {a}");
+            Console.WriteLine($"Second: {b}");
+        }
+         internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Pair<string, int> p1 = new Pair<string, int>();
+                p1.Add("bread", 30);
+                p1.Display();
+            }
+        }
+    }
+}
