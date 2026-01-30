727ccf32dc64b750b20d9bca8118b7c8cde82c68
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-01-30 16:09:41 +0200

    added h/w 12.01

---
+++ homework10.1/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace homework10._1
+{
+    enum StoreType
+    {
+        Grocery,
+        Household,
+        Clothing,
+        Footwear
+    }
+
+    class Store : IDisposable
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public StoreType Type { get; set; }
+
+        private bool disposed = false;
+
+        public Store(string name, string address, StoreType type)
+        {
+            Name = name;
+            Address = address;
+            Type = type;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Назва магазину: " + Name);
+            Console.WriteLine("Адреса: " + Address);
+            Console.WriteLine("Тип: " + Type);
+            Console.WriteLine();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Dispose: звільнення ресурсів для " + Name);
+                }
+                disposed = true;
+            }
+        }
+
+        ~Store()
+        {
+            Console.WriteLine("Деструктор для магазину: " + Name);
+            Dispose(false);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store1 = new Store("АТБ", "вул. Левітана 10", StoreType.Grocery);
+            store1.ShowInfo();
+            store1.Dispose();
+
+            Console.WriteLine("---");
+
+            using (Store store2 = new Store("Епіцентр", "вул. Небесної Сотні", StoreType.Household))
+            {
+                store2.ShowInfo();
+            }
+
+            Console.WriteLine("---");
+
+            Store store3 = new Store("Zara", "ТРЦ Ocean Plaza", StoreType.Clothing);
+            store3.ShowInfo();
+
+            Console.WriteLine("---");
+
+            using (Store store4 = new Store("Vito Rossi", "ТРЦ Lavina", StoreType.Footwear))
+            {
+                store4.ShowInfo();
+            }
+
+            Console.WriteLine(".");
+        }
+    }
+}
+
---
+++ homework10/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace homework10
+{
+    class Play
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Play(string title, string author, string genre, int releaseYear)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            ReleaseYear = releaseYear;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Назва: " + Title);
+            Console.WriteLine("Автор: " + Author);
+            Console.WriteLine("Жанр: " + Genre);
+            Console.WriteLine("Рік випуску: " + ReleaseYear);
+            Console.WriteLine();
+        }
+
+        ~Play()
+        {
+            Console.WriteLine("Деструктор викликано для: " + Title);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Play play1 = new Play(
+            "Лісова пісня",
+            "Леся Українка",
+            "Драма",
+            1911
+            );
+
+            Play play2 = new Play(
+            "Каменярі",
+            "Іван Франко",
+            "Соціальна драма",
+            1897
+            );
+
+            play1.ShowInfo();
+            play2.ShowInfo();
+
+            
+            Console.WriteLine("тест деструктора");
+
+            {
+                Play tempPlay = new Play(
+                    "Тимчасовий об'єкт",
+                    "Тест Автор",
+                    "Тест",
+                    2000
+                );
+                tempPlay.ShowInfo();
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+           
+            for (int i = 0; i < 3; i++)
+            {
+                Play shortPlay = new Play(
+                    $"П'єса {i + 1}",
+                    $"Автор {i + 1}",
+                    "Тест",
+                    2020 + i
+                );
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("\n.");
+        }
+    }
+}
+
