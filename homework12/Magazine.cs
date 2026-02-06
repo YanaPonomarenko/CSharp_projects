999d63377da2a621e48336de51a122003b0bad4e
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-02-06 14:10:53 +0200

    added h/w 16.01

---
+++ homework12/Magazine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework12
+{
+    public class Magazine
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Pages { get; set; }
+    }
+}
